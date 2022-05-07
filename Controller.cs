d830c747182b59b5a0aae3607121f85b09f24df9
Azamat-aka-Arzamir <102511234+Azamat-aka-Arzamir@users.noreply.github.com>
2022-05-07 19:14:09 +0300

    everything

---
+++ Controller.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+
+public class Controller : MonoBehaviour
+{
+    public Movement selfMove;
+    float x;
+    float y;
+    Vector2 input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        selfMove = GetComponent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        selfMove.Move(input);
+    }
+    public void GetMovement(InputAction.CallbackContext context)
+	{
+        x = context.ReadValue<Vector2>().x;
+        input = context.ReadValue<Vector2>();
+    }
+    public void GetJump(InputAction.CallbackContext context)
+	{
+		if (context.performed && !context.started)
+		{
+            selfMove.Jump();
+		}
+	}
+    public void GetDash(InputAction.CallbackContext context)
+	{
+        if (context.performed && !context.started)
+        {
+            selfMove.Dash();
+        }
+    }
+}
---
+++ Movement.cs
@@ -0,0 +1,220 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	Rigidbody2D SelfRB;
+	Collider2D SelfColl;
+	float mass;
+	[SerializeField] int JumpRemains;
+
+	[SerializeField] int Wall;
+	[SerializeField] bool OnGround;
+	[SerializeField] int JumpsCount;
+	[SerializeField] bool WallJumpAbility;
+	[SerializeField] bool DashAbility;
+	[SerializeField] bool OnWall;
+	[SerializeField] int DashCoolDown;
+	bool DashCD;
+	bool IsDashing;
+	bool SlideDown;
+	[SerializeField] int DashSpeed;
+	[SerializeField] int DashLength;
+	[SerializeField] int StaminaRegenerationSpeed;
+
+	[SerializeField] int MaxSpeed;
+	int LocalMaxspeed;
+	[SerializeField] float Acceleration;
+	[SerializeField] int JumpForce;
+	[SerializeField] float SlideSpeed;
+
+	[SerializeField] float BasicDamage;
+
+	[SerializeField] int DashCost;
+	[SerializeField] int JumpCost;
+	[SerializeField] int SlideCost;
+	[SerializeField] int AttackCost;
+	float JumpAnimationLength;
+	public int Bullets;
+	public int HealthPoints;
+	public int Stamina;
+	int StaminaRemains;
+	int lastDir;
+
+	float a;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		SelfRB = GetComponent<Rigidbody2D>();
+		SelfColl = GetComponent<Collider2D>();
+		JumpAnimationLength = GetJumpAnimationLength();
+		mass = SelfRB.mass;
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		RegenerateStamina();
+		Slide();
+		OnGround = CheckGround();
+	}
+	public void Move(Vector2 direction)
+	{
+		if (direction.y < -0.5)
+		{
+			SlideDown = true;
+		}
+		else
+		{
+			SlideDown = false;
+		}
+		if (!OnGround)
+		{
+			return;
+		}
+		Friction();
+		if (IsDashing)
+		{
+			return;
+		}
+		Vector2 force = direction * Acceleration * mass;
+		if (direction.x == 0)
+		{
+			LocalMaxspeed = 0;
+			if (Mathf.Abs(SelfRB.velocity.x) < 1f)
+			{
+				SelfRB.velocity = new Vector2(0, SelfRB.velocity.y);
+			}
+		}
+		else
+		{
+			lastDir =(int)Mathf.Sign(direction.x); 
+			LocalMaxspeed = MaxSpeed;
+		}
+		SelfRB.AddForce(force);
+	}
+	void Friction()
+	{
+		var k = Mathf.Abs(SelfRB.velocity.x) / (LocalMaxspeed + 1f);
+		if (k > 100) k = 100;
+		Vector2 frictionForce = Vector2.right * mass * Acceleration * -Mathf.Sign(SelfRB.velocity.x) * k;
+		SelfRB.AddForce(frictionForce);
+	}
+	bool CheckGround()
+	{
+		ContactPoint2D[] contacts = new ContactPoint2D[10];
+		SelfRB.GetContacts(contacts);
+		if (contacts[0].collider == null)
+		{
+			Wall = 0;
+			return false;
+		}
+		foreach (var contact in contacts)
+		{
+			if (contact.collider == null)
+			{
+				break;
+			}
+			if (contact.collider.tag == "Ground")
+			{
+				if (contact.normal.y > 0.9)
+				{
+					Wall = 0;
+					JumpRemains = JumpsCount;
+					return true;
+				}
+				else if (contact.normal.y == 0)
+				{
+					Wall = (int)contact.normal.x;
+				}
+			}
+		}
+		return false;
+	}
+	public void Jump()
+	{
+		StartCoroutine(IeJump());
+	}
+	IEnumerator IeJump()
+	{
+		if (JumpRemains != 0 && StaminaRemains >= JumpCost && !IsDashing)
+		{
+			StaminaRemains -= JumpCost;
+			yield return new WaitForSeconds(JumpAnimationLength);
+			var xSpeed = SelfRB.velocity.x;
+			SelfRB.velocity = new Vector2(xSpeed, 0);
+			if (Wall == 0)
+			{
+				SelfRB.AddForce(Vector2.up * JumpForce * mass);
+			}
+			else
+			{
+				SelfRB.AddForce(new Vector2(Wall * 0.5f, 1).normalized * JumpForce * mass);
+			}
+			yield return new WaitForFixedUpdate();
+			yield return new WaitForFixedUpdate();
+			JumpRemains--;
+		}
+		StopCoroutine(IeJump());
+		yield break;
+	}
+	float GetJumpAnimationLength()
+	{
+		float length = 0;
+		var animator = GetComponent<Animator>();
+		List<AnimationClip> clips = new List<AnimationClip>();
+		clips.AddRange(animator.runtimeAnimatorController.animationClips);
+		if (animator.HasState(0, Animator.StringToHash("Jump"))) length = clips.Find(x => x.name == "Jump").length - 0.1f;
+		return length;
+	}
+	void RegenerateStamina()
+	{
+		if (StaminaRemains < Stamina)
+		{
+			StaminaRemains += StaminaRegenerationSpeed;
+		}
+	}
+	public void Fire(Vector2 direction, Weapon weapon)
+	{
+
+	}
+	void Slide()
+	{
+		if (Wall == 0 || !WallJumpAbility || StaminaRemains < SlideCost || SlideDown)
+		{
+			return;
+		}
+		StaminaRemains -= SlideCost;
+		float frictionForce = -SelfRB.velocity.y / SlideSpeed * mass;
+		if (frictionForce > Physics2D.gravity.y * mass) frictionForce = Physics2D.gravity.y * mass;
+		if (SelfRB.velocity.y < -SlideSpeed) SelfRB.AddForce(Vector2.up * Physics2D.gravity.y * frictionForce);
+	}
+	public void Dash()
+	{
+		if (DashAbility && !DashCD && StaminaRemains > DashCost&&OnGround)
+		{
+			StaminaRemains -= DashCost;
+			DashCD = true;
+			StartCoroutine(IeDash(lastDir));
+		}
+	}
+	IEnumerator IeDash(float direction)
+	{
+		IsDashing = true;
+		for (int i = 0; i < DashLength; i++)
+		{
+			LocalMaxspeed = DashSpeed;
+			SelfRB.AddForce(Vector2.right * Mathf.Sign(direction) * 50);
+			yield return new WaitForFixedUpdate();
+		}
+		IsDashing = false;
+		LocalMaxspeed = MaxSpeed;
+		yield return new WaitForSeconds(DashCoolDown);
+		DashCD = false;
+		yield break;
+	}
+
+}
